6235fe0fddcb6fd3b4e776010bb20c0159f50a9f
ZaneCQ <897140741@qq.com>
2020-12-03 02:01:34 +0800

    Support react component and less files.

--- src/index.js
+++ src/index.js
@@ -1,29 +1,32 @@
+import React, { Component } from 'react';
+import ReactDom from 'react-dom';
+import { Button } from 'antd';
 import _ from 'lodash';
 import printMe from './print.js';
 import './styles.css';
 
-function component() {
-    var element = document.createElement('div');
-    var btn = document.createElement('button');
-
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
-    btn.innerHTML = 'Click me and check the console!';
-    btn.onclick = printMe;  // onclick event is bind to the original printMe function
-
-    element.appendChild(btn);
-
-    return element;
+class App extends Component {
+    onClick = () => {
+        console.log('button clicked')
+    }
+    render() {
+        let greeting = `Hi, welcome to my simple project😀`;
+        console.log('done')
+        return (
+            <div>
+                <h1>{greeting}</h1>
+                <div><Button onClick={this.onClick}>Click</Button></div>
+            </div>
+        )
+    }
 }
 
-let element = component();
-document.body.appendChild(element);
-
-if (module.hot) {
+ReactDom.render(<App />, document.getElementById('app'));
+/* if (module.hot) {
     module.hot.accept('./print.js', function () {
         console.log('Accepting the updated printMe module!');
         document.body.removeChild(element);
         element = component(); // Re-render the "component" to update the click handler
         document.body.appendChild(element);
     })
-}
+} */
--- webpack.common.js
+++ webpack.common.js
@@ -2,6 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const webpack = require('webpack');
+// console.log('======dirname=======:', __dirname);
 
 module.exports = {
   entry: {
@@ -13,14 +14,6 @@ module.exports = {
     publicPath: '/'
   },
   mode: 'development',
-  plugins: [
-    // new CleanWebpackPlugin(),
-    new HtmlWebpackPlugin({
-      title: 'Output Management'
-    }),
-    new webpack.NamedModulesPlugin(),
-    new webpack.HotModuleReplacementPlugin()
-  ],
   module: {
     rules: [
       {
@@ -29,48 +22,71 @@ module.exports = {
       },
       {
         test: /\.less$/,
-        use: ['style-loader', 'css-loader', 'postcss-loader', 'less-loader']
+        use: ['style-loader', 'css-loader', 'postcss-loader', {
+          loader: "less-loader",
+          options: {
+            lessOptions: {
+              javascriptEnabled: true
+            }
+          }
+        }]
       },
       {
-        test: /\.jsx$/,
+        test: /\.jsx?$/,
         exclude: /node_modules/,
-        loader: ['babel-loader']
-      }
-    ]
-  },
-  devtool: 'inline-source-map',
-  devServer: {
-    contentBase: './dist',
-    port: 8080,
-    open: true,
-    hot: true,
-  },
-};
-/* module.exports = {
-  entry: {
-    app: './src/index.js',
-    print: './src/print.js'
-  },
-  output: {
-    filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'dist')
-  },
-  mode: 'development',
-  plugins: [
-    new CleanWebpackPlugin(['dist']),
-    new HtmlWebpackPlugin({
-      title: 'Output Management'
-    })
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
         use: [
-          'style-loader',
-          'css-loader'
+          {
+            loader: 'babel-loader',
+            /* options: {
+              "presets": ["@babel/preset-env", "@babel/preset-react"],
+              "plugins": [
+                "@babel/plugin-transform-runtime",
+                "@babel/plugin-proposal-class-properties",
+                [
+                  "@babel/plugin-proposal-decorators",
+                  { "legacy": true }
+                ],
+                [
+                  "import",
+                  { "libraryName": "antd", "style": true }
+                ]
+              ]
+            } */
+          }
+        ]
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 8192
+            }
+          }
         ]
       }
     ]
-  }
-}; */
+  },
+  // presets: ['@babel/preset-env', '@babel/preset-react'],
+  plugins: [
+    // new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: 'My Project',
+      filename: 'index.html',
+      template: path.join(__dirname, 'src/index.html'),
+      inject: 'body',
+    }),
+    new webpack.HotModuleReplacementPlugin()
+  ],
+  optimization: {
+    namedModules: true
+  },
+  // devtool: 'inline-source-map',
+  // devServer: {
+  //   contentBase: './dist',
+  //   port: 8080,
+  //   open: true,
+  //   hot: true,
+  // },
+};
